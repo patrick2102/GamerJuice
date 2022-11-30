7c6e25b428cfe149da7ff973e6bf7e1772f48d88
Rasmus Klintsø <rall1812@gmail.com>
2022-11-30 13:16:36 +0100

    First version of basic speed increase system based in test scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameManager instance;
+    public static GameManager instance;
+    public static float speedMultiplier;
+    
+    public GameObject player;
     public float elapsedTime;
+    public float initSpeedMultiplier = 1f;
+    public int speedLevel;
+    public float speedIncreaseInterval = 5f;
+    public float speedIncreaseAmount = 0.2f;
     
     private void Awake()
     {
         instance = this;
         elapsedTime = 0f;
+        speedLevel = 1;
     }
 
     
     void Start()
     {
-        
+        speedMultiplier = initSpeedMultiplier;
     }
 
     
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        
+
+        if (elapsedTime > speedLevel * speedIncreaseInterval)
+        {
+            speedLevel++;
+            speedMultiplier += speedIncreaseAmount;
+            player.GetComponent<PlayerForwardController>().ApplySpeedMultiplier();
+        }
     }
 
     void Restart()
--- Assets/Scripts/PlayerForwardController.cs
+++ Assets/Scripts/PlayerForwardController.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerForwardController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameManager game;
+    private Rigidbody2D rbody;
+    private BoxCollider2D collider;
+    private SpriteRenderer sprite;
+    
+    
+    public Vector2 baseVelocity = new Vector2(1f, 0f);
+    public Vector2 currentVelocity;
+    public float speed;
+
+    private void Awake()
+    {
+        game = GameManager.instance;
+        rbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
+    {
+        speed = baseVelocity.x;
+        currentVelocity = baseVelocity;
+    }
+
+    
+    void Update()
+    {
+        currentVelocity = new Vector2(speed, rbody.velocity.y);
+        rbody.velocity = currentVelocity;
+    }
+
+    private void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ApplySpeedMultiplier()
     {
-        
+        speed = GameManager.speedMultiplier;
     }
 }
