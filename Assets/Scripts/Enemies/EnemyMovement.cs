159d936dbecec35b67ebcd3c1cdad0017f3ffe69
JPDC20 <jpdconstantino20@gmail.com>
2022-12-28 10:27:09 +0100

    Ranged enemy Gizmo

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField]
     float speed = 1.5f;
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Enemies/EnemyShoting.cs
+++ Assets/Scripts/Enemies/EnemyShoting.cs
@@ -13,6 +13,8 @@ public class EnemyShoting : MonoBehaviour
     [SerializeField]
     float projectile_speed;
     [SerializeField]
+    float range;
+    [SerializeField]
     float shooting_cd;
     float shooting_timer;
 
@@ -25,20 +27,29 @@ public class EnemyShoting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        shooting_timer += Time.deltaTime;
-            
-        var aiming_direction = (target.transform.position - this.transform.position).normalized;
-        aim.transform.position = this.transform.position + aiming_direction;
+        if((target.transform.position - this.transform.position).magnitude <= range){
+            shooting_timer += Time.deltaTime;
+                
+            var aiming_direction = (target.transform.position - this.transform.position).normalized;
+            aim.transform.position = this.transform.position + aiming_direction;
 
-        if(shooting_timer >= shooting_cd){
-            float dot = Vector2.Dot(transform.right, aiming_direction);
-            dot = Mathf.Acos(dot);
-            float angles = 180 - dot * 180 / Mathf.PI;
-            var projectile_position = aim.transform.position;
-            GameObject projectile = Instantiate(projectile_prefab, projectile_position, Quaternion.Euler(0, 0, angles));
-            projectile.GetComponent<ArrowMovement>().setArrowMovement(aiming_direction, projectile_speed);
-            shooting_timer = 0;
+            if(shooting_timer >= shooting_cd){
+                float dot = Vector2.Dot(transform.right, aiming_direction);
+                dot = Mathf.Acos(dot);
+                float angles = 180 - dot * 180 / Mathf.PI;
+                var projectile_position = aim.transform.position;
+                GameObject projectile = Instantiate(projectile_prefab, projectile_position, Quaternion.Euler(0, 0, angles));
+                projectile.GetComponent<ArrowMovement>().setArrowMovement(aiming_direction, projectile_speed);
+                shooting_timer = 0;
+            }
         }
+    }
+
+    private void OnDrawGizmos() {
+        
+        Gizmos.color = Color.red;
+        //Gizmos.DrawLine(transform.position, transform.position + (target.transform.position - this.transform.position).normalized * range);
+        Gizmos.DrawWireSphere(transform.position, range);
     }
     
 
