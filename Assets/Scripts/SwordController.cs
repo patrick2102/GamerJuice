30f5c9a87f6bfc19256b08c04ea6d3e433c3c3c3
paco <paco@itu.dk>
2022-12-28 15:16:00 +0100

    Sword is functional but

--- Assets/Scripts/SwordController.cs
+++ Assets/Scripts/SwordController.cs
@@ -17,6 +17,7 @@ public class SwordController : MonoBehaviour
 
     [SerializeField] Rigidbody2D playerRb;
     [SerializeField] Transform playerTransform;
+    [SerializeField] PlayerForwardController pfc;
     Vector3 lastVelocity;
 
 
@@ -35,15 +36,22 @@ public class SwordController : MonoBehaviour
         {
             rb = GetComponent<Rigidbody2D>();
         }
+        if (pfc == null)
+        {
+            pfc = GetComponentInParent<PlayerForwardController>();
+        }
 
         distFromPlayer = (playerTransform.position - transform.position).magnitude;
+
+        Physics.IgnoreCollision(playerRb.GetComponent<Collider>(), GetComponent<Collider>());
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //MoveSword();
+
     }
 
     void FixedUpdate()
@@ -53,12 +61,6 @@ public class SwordController : MonoBehaviour
 
     void MoveSword()
     {
-        // Vector from player mouse. Used for calculating the target position for the sword.
-        var playerToMouse = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        playerToMouse.z = 0.0f;
-        playerToMouse -= playerTransform.position;
-        playerToMouse = playerToMouse.normalized * distFromPlayer;
-
         //Convert rigidbody velocity to 3D vector
         var velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
 
@@ -66,45 +68,64 @@ public class SwordController : MonoBehaviour
         var leftOverVelocity = (velocity - lastVelocity) * 0.5f;
         playerRb.AddForce(leftOverVelocity, ForceMode2D.Impulse);
 
+
+
+
+        // Vector from player mouse. Used for calculating the target position for the sword.
+        var playerToMouse = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        playerToMouse.z = 0.0f;
+        playerToMouse -= playerTransform.position;
+        playerToMouse = playerToMouse.normalized * distFromPlayer;
+
+
         // The target is the vector from the sword to the mouse.
         // Defined as the vector from the sword endpoint at the next frame, to the vector towards the mouse position.
-        //var target = playerToMouse - (transform.position + velocity * Time.deltaTime) + playerTransform.position;
 
-        var nextPos = transform.position + velocity * Time.deltaTime;
+        //The position of the sword tip at the next frame
+        var fromPos = transform.position + velocity * Time.deltaTime;
+        //var fromPos = transform.position;
 
-        var target = playerToMouse + playerTransform.position;
+        //The position of the end of movement
+        var targetPos = playerToMouse + playerTransform.position;
 
-        var towards = speed * Time.deltaTime * (target - nextPos).normalized;
-        var dist = (target - nextPos);
+        var towards = speed * (targetPos - fromPos).normalized;
+        var dist = (targetPos - fromPos);
 
         // Slowdown is the 
         var slowdown = velocity * (1 - math.min(dist.magnitude / slowDownRadius, 1));
 
-        var movement = towards - slowdown;
+        var movement = (towards - slowdown) * Time.deltaTime;
+        //var movement = towards;
+        //movement += 
+
+        //movement += addVelocity;
+        //rbody.AddForce(addVelocity);
+        //movement += playerRb.velocity;
+        //movement += playerRb.velocity;
+        //rb.velocity = playerRb.velocity;
+
 
         if ((playerToMouse + playerTransform.position - transform.position).magnitude < stopRadius)
         {
             rb.velocity = Vector2.zero;
-        } 
+        }
         else
         {
+            //Vector2 addVelocity = new Vector2(pfc.topSpeed, 0f);
+            //rb.AddForce(movement, ForceMode2D.Impulse);
+            Vector3 addVelocity = new Vector2(pfc.topSpeed, 0f);
+            rb.AddForce(addVelocity);
             rb.AddForce(movement, ForceMode2D.Impulse);
+            //rb.AddForce(movement);
+            //rb.AddForce(addVelocity);
+
         }
 
         lastVelocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
 
+        var angle = Vector2.SignedAngle(Vector2.right, new Vector2(transform.localPosition.x, transform.localPosition.y).normalized);
 
-        //var angle = Vector3.SignedAngle(transform, playerToMouse, Vector3.);
-
-
-        //
-        //Debug.Log("angle: " + angle);
-
-        //transform.rotation = Quaternion.Euler(0, 0, 30);
-
-        //transform.rotation = Quaternion.Euler(0, 0, angle);
-
-        //transform.rotation.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
 
         if (debugMode)
         {
